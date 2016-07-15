75453de2f74bf33dd6f56b353c1c0c87f9f86b3d
manishkumarmdb <manishkumarmdb@gmail.com>
2016-07-15 11:55:21 +0530

    cljsjs

---
+++ resources/public/js/vendor/myexterns.js
@@ -0,0 +1,17 @@
+//var chance = {
+//  "bool" : function() {}
+//}
+
+/*
+var chance = {
+  "character" : function() {}
+} */
+
+
+//var chance0 = new Chance(
+//  "bool" = function() {}
+//);
+
+var chance1 = new Chance({
+  "random" : function() {}
+});
